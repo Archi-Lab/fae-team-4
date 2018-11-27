cb161bb075eea7b66397e5aee15a358354a95701
Franzi Bieg <franzi.bieg@live.de>
2018-11-27 10:50:32 +0100

    id bei Filterung entfernt, erste Überlegung zur Umkreissuche hinzugefügt

--- DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/controller/DvpPositionController.java
+++ DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/controller/DvpPositionController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
+import java.util.OptionalLong;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -24,10 +25,10 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 public class DvpPositionController {
 
     private final DvpRepository dvpRepository;
+    private long erdradius = 6371;
 
     @Autowired
-    public DvpPositionController(
-            DvpRepository dvpRepository) {
+    public DvpPositionController( DvpRepository dvpRepository) {
         this.dvpRepository = dvpRepository;
     }
 
@@ -54,15 +55,37 @@ public class DvpPositionController {
         return  ResponseEntity.ok(resource);
     }
 
-    @GetMapping("/{id}/filter")
-    public ResponseEntity<?> getDVPFilter(@PathVariable("id") long id,
-                                          @RequestParam("lat") long lat,
+    @GetMapping("/filter")
+    public ResponseEntity<?> getDVPFilter(@RequestParam("lat") long lat,
                                           @RequestParam("lon") long lon,
-                                          @RequestParam("radius") long radius)
+                                          @RequestParam("radius") OptionalLong rad)
     {
-        // http://localhost:8081/dvps/1/filter?lat=1&lon=2&radius=10
+        // Beispiel: http://localhost:8081/dvps/filter?lat=1&lon=2&radius=10
 
-        final DVP dvp = new DVP(radius);
+        // TODO Dummy rauswerfen
+        if(rad.isPresent()){
+
+            long radius = rad.getAsLong();
+
+            // latitude Grenzen
+            double maxLat = lat + Math.toDegrees(radius/erdradius);
+            double minLat = lat - Math.toDegrees(radius/erdradius);
+
+            // longitude Grenzen
+            double maxLon = lon + Math.toDegrees(radius/erdradius/Math.cos(Math.toRadians(lat)));
+            double minLon = lon - Math.toDegrees(radius/erdradius/Math.cos(Math.toRadians(lat)));
+
+            // todo filter zuende
+
+        }
+        else {
+
+            // todo kein Radius wurde angegeben, nur für die bestimmte lon und lat suchen
+
+        }
+
+        // Alt
+        final DVP dvp = new DVP(rad.getAsLong());
 
         dvp.AddAufenthaltsort(new Aufenthaltsort(new Date(), new Position(lat, lon)));
 
@@ -70,7 +93,7 @@ public class DvpPositionController {
 
         // todo: Filtern
 
-        resource.add(linkTo(methodOn(DvpPositionController.class).getDVPFilter(id, lat, lon, radius)).withSelfRel());
+        resource.add(linkTo(methodOn(DvpPositionController.class).getDVPFilter(lat, lon, rad)).withSelfRel());
 
         return  ResponseEntity.ok(resource);
     }
