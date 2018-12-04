5b259901a8d349d2fd985ba2b60797256bf4f6c1
Franzi Bieg <franzi.bieg@live.de>
2018-12-04 16:14:06 +0100

    Änderungen sql statement

--- DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/repositories/CustomDvpRepositoryImpl.java
+++ DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/repositories/CustomDvpRepositoryImpl.java
@@ -26,7 +26,7 @@ public class CustomDvpRepositoryImpl implements CustomDvpRepository {
 
     public Iterable<DVP> findAllByUmkreissuche(double lat, double lng, long radius){
 
-        final Query query = entityManager.createNativeQuery("SELECT d.* FROM DVP as d WHERE EXISTS (SELECT NULL FROM Aufenthaltsort as a WHERE a.DVP_id = d.id AND 6371 * (ACOS(SIN(RADIANS(d.Latitude)) * SIN(RADIANS(?1)) + COS(RADIANS(d.Latitude)) * COS(RADIANS(?1)) * COS(RADIANS(d.Longitude) - RADIANS(?2)))) < ?3 )", DVP.class);
+        final Query query = entityManager.createNativeQuery("SELECT d.* FROM DVP as d WHERE EXISTS (SELECT NULL FROM Aufenthaltsort as a WHERE a.DVP_id = d.id AND 6371 * (ACOS(SIN(RADIANS(d.Latitude)) * SIN(RADIANS(:1)) + COS(RADIANS(d.Latitude)) * COS(RADIANS(:1)) * COS(RADIANS(d.Longitude) - RADIANS(:2)))) < :3 )", DVP.class);
 
         query.setParameter(1, lat);
         query.setParameter(2, lng);
