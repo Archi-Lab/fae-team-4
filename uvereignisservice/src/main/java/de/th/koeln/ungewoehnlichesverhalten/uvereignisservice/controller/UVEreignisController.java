300ad0c624c5f729b6a1b81ed1930830f78a1d63
Selina M <selina.magnin@web.de>
2018-12-04 14:32:50 +0100

    UVEController geaendert, Code an Sebastians angepasst

--- uvereignisservice/src/main/java/de/th/koeln/ungewoehnlichesverhalten/uvereignisservice/controller/UVEreignisController.java
+++ uvereignisservice/src/main/java/de/th/koeln/ungewoehnlichesverhalten/uvereignisservice/controller/UVEreignisController.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.ResourceProcessor;
+import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,9 @@ import java.util.Optional;
 import de.th.koeln.ungewoehnlichesverhalten.uvereignisservice.models.UVEreignis;
 import de.th.koeln.ungewoehnlichesverhalten.uvereignisservice.repositories.UVEreignisRepository;
 
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
 @RepositoryRestController
 public class UVEreignisController {
 
@@ -26,20 +30,57 @@ public class UVEreignisController {
         this.uvEreignisRepository = uvEreignisRepository;
     }
 
-    @GetMapping(path = "/uvereignisse/{eId}")
-    public Optional<UVEreignis> getUVEreignis(@PathVariable("eId") Long id) {
-        return this.uvEreignisRepository.findById(id);
+    /**
+     * GET auf alle UVEreignisse
+     * @return
+     */
+    @GetMapping()
+    public ResponseEntity<?> getUVEreignisse() {
+        final Iterable<UVEreignis> uvEreignis = this.uvEreignisRepository.findAll();
+        Resources<UVEreignis> resources = new Resources<>(uvEreignis);
+        resources.add(linkTo(methodOn(UVEreignisController.class).getUVEreignisse()).withSelfRel());
+        return ResponseEntity.ok(resources);
     }
 
-    @PostMapping(path = "/uvereignisse/{eId}/sprachnachricht")
+    //TODO: POST uvereignisse
+
+    /**
+     * GET auf bestimmtes UVEreignis (by ID)
+     * @param eid
+     * @return
+     */
+    @GetMapping(path = "/uvereignisse/{eId}")
+    public ResponseEntity<?> getUVEreignis (@PathVariable("eId") long eid) {
+        final Optional<UVEreignis> uvEreignis = this.uvEreignisRepository.findById(eid);
+        Resource<Optional<UVEreignis>> uvEreignisRessource = new Resource<>(uvEreignis);
+        uvEreignisRessource.add(linkTo(methodOn(UVEreignisController.class).getUVEreignis(eid)).withSelfRel());
+        return ResponseEntity.ok(uvEreignisRessource);
+    }
+
+    //TODO: PATCH/PUT /uvereignisse/{eId}
+    //TODO: PUT /uvereignisse/{eId}/senden
+
+    /**
+     * DELETE auf bestimmtes UVEreignis (by ID)
+     * @param eid
+     */
+    @DeleteMapping(path = "/uvereignisse/{eId}")
+    public void deleteUVEreignis(@PathVariable("eId") long eid) {
+        this.uvEreignisRepository.deleteById(eid);
+    }
+
+    /**
+     * Post einer Sprachnachricht auf bestimmtes UVEreignis (by ID)
+     */
+    /*@PostMapping(path = "/uvereignisse/{eId}/sprachnachricht")
     public ResponseEntity<?> postSprachnachricht(@PathVariable("eId") Long id) {
         final Optional<UVEreignis> optionalUVEreignis = this.uvEreignisRepository.findById(id);
         if (optionalUVEreignis.isPresent()) {
             final UVEreignis uvEreignis = optionalUVEreignis.get();
             //final Sprachnachricht sprachnachricht = uvEreignis.getSprachnachricht();
-            /*Macht irgendwie keinen Sinn, weil durch das ändern des UVE die Sprachnachricht ja automatisch geändert wird*/
+            //Macht irgendwie keinen Sinn, weil durch das ändern des UVE die Sprachnachricht ja automatisch geändert wird
             try {
-                uvEreignis.setSprachnachricht(sprachnachricht); /*zuerst get und hier setze ich sie dann wieder?*/
+                uvEreignis.setSprachnachricht(sprachnachricht); //zuerst get und hier setze ich sie dann wieder?
                 this.uvEreignisRepository.save(uvEreignis);
                 return ResponseEntity.ok(new Resource<>(uvEreignis));
             } catch (Exception e) {
@@ -48,17 +89,20 @@ public class UVEreignisController {
         } else {
             return ResponseEntity.notFound().build();
         }
-    }
+    }*/
 
-    @PostMapping(path = "/uvereignisse/{eId}/danke")
+    /**
+     * Post einer Danke-Nachricht auf bestimmtes UVEreignis (by ID)
+     */
+    /*@PostMapping(path = "/uvereignisse/{eId}/danke")
     public ResponseEntity<?> postDankenachricht(@PathVariable("eId") Long id) {
         final Optional<UVEreignis> optionalUVEreignis = this.uvEreignisRepository.findById(id);
         if (optionalUVEreignis.isPresent()) {
             final UVEreignis uvEreignis = optionalUVEreignis.get();
             //final Dankenachricht dankenachricht = uvEreignis.getDankenachricht();
-            /*Macht irgendwie keinen Sinn, weil durch das ändern des UVE die Dankenachricht ja automatisch geändert wird*/
+            //Macht irgendwie keinen Sinn, weil durch das ändern des UVE die Dankenachricht ja automatisch geändert wird
             try {
-                uvEreignis.setDankenachricht(dankenachricht); /*zuerst get und hier setze ich sie dann wieder?*/
+                uvEreignis.setDankenachricht(dankenachricht); //zuerst get und hier setze ich sie dann wieder?
                 this.uvEreignisRepository.save(uvEreignis);
                 return ResponseEntity.ok(new Resource<>(uvEreignis));
             } catch (Exception e) {
@@ -67,12 +111,10 @@ public class UVEreignisController {
         } else {
             return ResponseEntity.notFound().build();
         }
-    }
+    }*/
 
-    @DeleteMapping(path = "/uvereignisse/{eId}")
-    public void deleteUVEreignis(@PathVariable("eId") Long id) {
-        this.uvEreignisRepository.deleteById(id);
-    }
+    //TODO: GET /uvereignisse/{eId}/danke/{dId}
+    //TODO: GET /uvereignisse/{eId}/danke
 
     @Bean
     public ResourceProcessor<Resource<UVEreignis>> UVEreignisProcessor() {
