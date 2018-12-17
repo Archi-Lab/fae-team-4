e8f7353d41fee98f5696c3af08308932a5b46cf3
sevogt <sebastian.vogt@smail.th-koeln.de>
2018-12-17 19:26:32 +0100

    Customize REST path for Aufenthaltsorte Repository to use German plural

--- DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/repositories/AufenthaltsorteRepository.java
+++ DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/repositories/AufenthaltsorteRepository.java
@@ -2,8 +2,10 @@ package de.th.koeln.fae.ungewoehnlichesverhalten.DVP.repositories;
 
 import de.th.koeln.fae.ungewoehnlichesverhalten.DVP.models.Aufenthaltsort;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.stereotype.Repository;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "aufenthaltsorte", path = "aufenthaltsorte")
 public interface AufenthaltsorteRepository extends CrudRepository<Aufenthaltsort, Long> {
 }
