2109c20979ef4ecab4c541cf7821e7c8bdc40fc7
sevogt <sebastian.vogt@smail.th-koeln.de>
2019-01-10 19:46:20 +0100

    Update consumer

--- DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/models/Aufenthaltsort.java
+++ DVP/src/main/java/de/th/koeln/fae/ungewoehnlichesverhalten/DVP/models/Aufenthaltsort.java
@@ -1,7 +1,5 @@
 package de.th.koeln.fae.ungewoehnlichesverhalten.DVP.models;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
